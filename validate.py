6a2ade2e8a305abe01c71a6dc0004a68f33b129b
Sudharshan V <sudarshan61kv@gmail.com>
2026-01-07 17:33:53 +0530

    removed unwanted files and edited scripts

--- validate.py
+++ validate.py
@@ -1,25 +1,30 @@
-file = open("followers.txt", "r")
-follow_list = file.readlines()
+import json
 
-for i in range(len(follow_list)):
-    follow_list[i] = follow_list[i].strip()
-file.close()
-file = open("following.txt", "r")
-following_list = file.readlines()
+with open("dontfollowback.json", "r") as not_followers_file:
+    not_followers = json.load(not_followers_file)
+with open("celebrities.json", "r") as celebrities_file:
+    celebrities = json.load(celebrities_file)
+    celebrities_usernames = [x["username"] for x in celebrities]
 
-for i in range(len(following_list)):
-    following_list[i] = following_list[i].strip()
-file.close()
-
-not_followed_list = []
-
-for person in following_list:
-    if person not in follow_list:
-        not_followed_list.append(person)
-
-for person in not_followed_list:
-    print(person)
-
-print(len(not_followed_list))
-
-
+for account in not_followers["dontFollowMeBack"]:
+    if account["username"] not in celebrities_usernames:
+        print()
+        print("Not a celebrity")
+        print("Username:", account["username"])
+        print("Name:", account["full_name"])
+        print("Link:", f"https://www.instagram.com/{account["username"]}/")
+        inp = input("Add this to celebrity (y/n) or s to stop till here: ")
+        if inp == "y":
+            celebrities.append(account)
+            print("Celebrity Added")
+            print()
+        elif inp == "s":
+            with open("celebrities.json", "w") as celebrities_file:
+                json.dump(celebrities, celebrities_file, indent=2)
+                print("Celebrities Updated")
+            exit()
+        else:
+            continue
+with open("celebrities.json", "w") as celebrities_file:
+    json.dump(celebrities, celebrities_file, indent=2)
+    print("Celebrities Updated")
